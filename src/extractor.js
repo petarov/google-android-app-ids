9f65119e35a6bba38f81381a290bdf117e56b767
Petar Petrov <ppetrov@vexelon.net>
2022-10-30 09:55:21 +0100

    Add js script that extracts app ids from the play store
    You have to be logged in and execute the script from inside the
    browser's console.

---
+++ src/extractor.js
@@ -0,0 +1,32 @@
+const scrollingElement = (document.scrollingElement || document.body);
+scrollingElement.scrollTop = scrollingElement.scrollHeight;
+const fnScroll = () => {
+    scrollingElement.scrollTop = scrollingElement.scrollHeight;
+    setTimeout(fnClick, 2000);
+};
+const fnClick = () => {
+    const btn = document.querySelector("button[aria-label='Show more']");
+    if (btn) {
+        btn.click();
+        setTimeout(fnScroll, 2000);
+    } else {
+        setTimeout(fnExtr, 2000);
+    }
+};
+const fnExtr = () => {
+    const rows = ["PackageName"];
+    const ahrefs = document.querySelectorAll("a[jslog]");
+    for (const ahref of ahrefs) {
+        rows.push(ahref.href.replace('https://play.google.com/store/apps/details?id=',''));
+    }
+    console.info('Apps found: ', rows.length);
+    setTimeout(() => {
+        console.log(rows.join("\n"));
+        const link = document.createElement("a");
+        link.setAttribute("href", "data:text/csv;charset=utf-8," + encodeURIComponent(rows.join("\n")));
+        link.setAttribute("download", "app-ids.csv");
+        document.body.appendChild(link);
+        link.click();
+    }, 1000);
+};
+setTimeout(fnScroll, 250);
